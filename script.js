9dee5ca57dfaf783582d81072d627bd562b8bb5d
aquaclara <71489659+aquaclara@users.noreply.github.com>
2020-09-18 16:58:14 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const iframe = document.getElementById('frame');
+  const fab = document.getElementById('fab');
+  const caption = document.getElementById('caption');
+  let menuIsVisible = false;
+
+  function collapseMenu() {
+    menuIsVisible = false;
+    iframe.classList.remove('with-menu');
+    fab.classList.remove('right');
+  }
+
+  function showMenu() {
+    menuIsVisible = true;
+    iframe.classList.add('with-menu');
+    fab.classList.add('right');
+  }
+
+  let loadCount = 0;
+  iframe.onload = (event) => {
+    if (loadCount++ < 1) return;
+    iframe.classList.add('loaded');
+    caption.classList.add('loaded');
+    iframe.classList.remove('with-menu');
+    iframe.onload = null;
+    fab.style['opacity'] = '1';
+
+    fab.onclick = (event) => {
+      if (menuIsVisible) collapseMenu();
+      else showMenu();
+    };
+
+    collapseMenu();
+  };
+});
